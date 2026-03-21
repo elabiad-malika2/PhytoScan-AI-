285427ee0b6c905a3580c678de7b0fed34420668
elabiad-malika2 <elabiadmalika64@gmail.com>
2026-03-21 23:27:16 +0000

    ajout de service de chunker

--- backend/app/services/rag/chunker.py
+++ backend/app/services/rag/chunker.py
@@ -1,1 +1,27 @@
-# Découpage des documents en chunks
+import re
+
+def chunk_phytoscan_text(full_text: str):
+    """Découpe le texte par maladie et prépare les métadonnées pour ChromaDB."""
+    print(" Découpage du texte en cours...")
+    raw_chunks = re.split(r'\n(?=[IVX]+\.\s)', full_text)
+    
+    documents = []
+    metadatas = []
+    ids = []
+    
+    for i, chunk in enumerate(raw_chunks):
+        chunk = chunk.strip()
+        if len(chunk) < 50: 
+            continue # Ignorer l'intro
+            
+        titre = "Général"
+        match = re.search(r'^[IVX]+\.\s(.*?)\n', chunk)
+        if match:
+            titre = match.group(1).strip()
+            
+        documents.append(chunk)
+        metadatas.append({"maladie": titre, "source": "Guide PhytoScan"})
+        ids.append(f"maladie_chunk_{i}")
+        
+    print(f" {len(documents)} maladies chunkées.")
+    return documents, metadatas, ids
