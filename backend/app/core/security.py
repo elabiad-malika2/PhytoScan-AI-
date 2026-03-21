6182f9064f630e26481a24c2c1d603b9f33cf1a3
elabiad-malika2 <elabiadmalika64@gmail.com>
2026-03-21 23:35:17 +0000

    ajout de code de security auth founction

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -1,1 +1,33 @@
 # Hachage mots de passe & JWT
+import bcrypt
+from datetime import datetime, timedelta
+from jose import jwt
+from app.core.config import settings
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    """Vérifie si le mot de passe correspond au hash."""
+    try:
+        return bcrypt.checkpw(
+            plain_password.encode('utf-8'),
+            hashed_password.encode('utf-8')
+        )
+    except ValueError:
+        return False
+
+def get_password_hash(password: str) -> str:
+    """Hashe le mot de passe avec bcrypt pur."""
+    salt = bcrypt.gensalt()
+    hashed = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed.decode('utf-8')
+
+def create_access_token(data: dict, expires_delta: timedelta = None):
+    """Crée le token JWT."""
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=1440)
+        
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+    return encoded_jwt
