5a2ddd9619a0fa11ede5d2c905128f819b48e779
elabiad-malika2 <elabiadmalika64@gmail.com>
2026-03-21 23:34:46 +0000

    ajout de handler error

--- backend/app/core/exceptions.py
+++ backend/app/core/exceptions.py
@@ -1,1 +1,25 @@
 # Custom handlers (ex: VisionModelError)
+
+class PhytoScanException(Exception):
+    """Classe de base. Permet de définir un message, un code HTTP et des headers"""
+    def __init__(self, detail: str, status_code: int = 400, headers: dict = None):
+        self.detail = detail
+        self.status_code = status_code
+        self.headers = headers
+
+class InvalidTokenException(PhytoScanException):
+    """Exception spécifique pour un JWT invalide, expiré ou manquant"""
+    def __init__(self):
+        super().__init__(
+            detail="Token invalide ou expiré. Veuillez vous reconnecter.",
+            status_code=401,
+            headers={"WWW-Authenticate": "Bearer"}
+        )
+
+class EmailAlreadyExistsException(PhytoScanException):
+    def __init__(self, email: str):
+        super().__init__(detail=f"L'email {email} est déjà utilisé.", status_code=400)
+
+class CredentialsException(PhytoScanException):
+    def __init__(self):
+        super().__init__(detail="Email ou mot de passe incorrect.", status_code=401)
