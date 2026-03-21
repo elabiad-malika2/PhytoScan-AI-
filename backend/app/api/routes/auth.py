0208a80801680b2bf5fed921ff983b70c97955f8
elabiad-malika2 <elabiadmalika64@gmail.com>
2026-03-21 23:36:03 +0000

    ajout des routes de auth

--- backend/app/api/routes/auth.py
+++ backend/app/api/routes/auth.py
@@ -1,1 +1,20 @@
 # Routes d'authentification
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from fastapi.security import OAuth2PasswordRequestForm
+
+from app.api.deps import get_db
+from app.schemas.user_schema import UserCreate, UserResponse
+from app.schemas.auth_schema import Token
+from app.dal.user_dal import register_new_user, authenticate_user
+
+router = APIRouter()
+
+@router.post("/register", response_model=UserResponse)
+def register(user_in: UserCreate, db: Session = Depends(get_db)):
+    new_user = register_new_user(db, user_in)
+    return new_user
+
+@router.post("/login", response_model=Token)
+def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    return authenticate_user(db, form_data.username, form_data.password)
