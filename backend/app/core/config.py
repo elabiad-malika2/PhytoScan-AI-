5da8de8f3deda61c235bea5bf725f9be02cba989
elabiad-malika2 <elabiadmalika64@gmail.com>
2026-03-21 23:34:22 +0000

    ajout de fonctonnalite de confiq pour key env

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,1 +1,24 @@
-# Variables d'environnement (Pydantic Settings)
+from pydantic_settings import BaseSettings,SettingsConfigDict
+
+class Settings(BaseSettings):
+    DATABASE_URL: str
+    SECRET_KEY: str
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 1440
+
+    # Ajouter pour Postgres
+    POSTGRES_USER: str =None
+    POSTGRES_PASSWORD: str=None
+    POSTGRES_DB: str=None
+    POSTGRES_HOST: str=None
+    POSTGRES_PORT: int=None
+
+    GEMINI_API_KEY:str
+    GROQ_API_KEY: str
+
+    MLFLOW_TRACKING_URI:str
+    OPENAI_API_KEY:str
+
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
+
+settings = Settings()
