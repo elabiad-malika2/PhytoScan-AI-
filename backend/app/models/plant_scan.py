8b8f3670f4d54d4f1188768d5ee9f84e0ac1901b
elabiad-malika2 <elabiadmalika64@gmail.com>
2026-03-21 23:14:49 +0000

    model plant

--- backend/app/models/plant_scan.py
+++ backend/app/models/plant_scan.py
@@ -1,1 +1,18 @@
 # Modèle PlantScan
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
+from sqlalchemy.sql import func
+from sqlalchemy.orm import relationship
+from app.db.base import Base
+
+class PlantScan(Base):
+    __tablename__ = "plant_scans"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
+    image_path = Column(String, nullable=False) # Où est sauvegardée la photo
+    disease_detected = Column(String, nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+
+    user = relationship("User", back_populates="scans")
+    # uselist=False indique que c'est une relation 1-à-1 (1 scan = 1 rapport)
+    report = relationship("Report", back_populates="scan", cascade="all, delete-orphan", uselist=False)
