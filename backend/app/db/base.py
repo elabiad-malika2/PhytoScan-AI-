dd317d803fc7573a8ada0f683c480b08c4b9c84e
elabiad-malika2 <elabiadmalika64@gmail.com>
2026-03-21 23:15:43 +0000

    ajoout de base de database

--- backend/app/db/base.py
+++ backend/app/db/base.py
@@ -1,1 +1,10 @@
-# Base déclarative SQLAlchemy
+from sqlalchemy.orm import declarative_base
+
+# Création de la base déclarative (que toutes les tables utilisent)
+Base = declarative_base()
+
+
+from app.models.user import User
+from app.models.plant_scan import PlantScan
+from app.models.query import Query
+from app.models.report import Report
